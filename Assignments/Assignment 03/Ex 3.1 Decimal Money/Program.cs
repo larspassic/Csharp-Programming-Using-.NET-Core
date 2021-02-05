570cdac0df2fe579ccea3a0cafc00303c3ee9911
Lars Passic <larspassic@outlook.com>
2021-02-04 19:30:31 -0800

    Use instructor's provided solution as starting point for Assignment 03
    Actually got it right this time - init

---
+++ Assignments/Assignment 03/Ex 3.1 Decimal Money/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+//Assignment 
+//Author: Passic, Lars, 2011958
+
+namespace Ex_3._1_Decimal_Money
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PurchasePrice sodaPrice = new PurchasePrice(35);
+
+            Console.WriteLine("Welcome to the .NET C# Soda Vending Machine");
+            Console.Write("Please insert {0} cents: ",sodaPrice.Price);
+            int valueInserted = int.Parse(Console.ReadLine());
+            Console.WriteLine("You have inserted {0} cents", valueInserted);
+            Console.WriteLine("Thanks, here is your soda.");
+        }
+    }
+}
---
+++ Assignments/Assignment 03/Ex 3.1 Decimal Money/PurchasePrice.cs
@@ -0,0 +1,39 @@
+﻿
+//Assignment 
+//Author: Passic, Lars, 2011958
+
+
+namespace Ex_3._1_Decimal_Money
+{
+    //  This class represents the purchase price of something.
+    //  In our software project, we will use it to represent the price of
+    //  one can of soda.
+    public class PurchasePrice
+    {
+        private int price = 0;
+
+        // This constructor sets the purchase price to zero
+        public PurchasePrice()
+        {
+            price = 0; 
+        }
+        // This constructor allows a new purchase price to be set by the user
+        public PurchasePrice(int initialPrice)
+        {
+            price = initialPrice;
+        }
+        //  This property gets and sets the value the purchase price.
+        public int Price
+        {
+            get
+            {
+                return price;
+            }
+            set
+            {
+                price = value;
+            }
+        }
+    } //end PurchasePrice
+
+}
