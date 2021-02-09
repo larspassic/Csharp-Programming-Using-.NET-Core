d7b29b1954cd4c0fe74033e38170d5707d4fd471
Lars Passic <43658463+larspassic@users.noreply.github.com>
2021-02-08 21:48:38 -0800

    Worked on comments for Can.cs
    Not sure what to do next.

--- Assignments/Assignment 03/Ex 3.2 Can Class/Can.cs
+++ Assignments/Assignment 03/Ex 3.2 Can Class/Can.cs
@@ -11,11 +11,13 @@ namespace Ex_3_2_Can_Class
         //It looks like this is initializing TheFlavor and defaulting it to REGULAR
         public readonly Flavor TheFlavor = Flavor.REGULAR;
 
+        //I don't know what this does. I think I need to make a get/set but I am not sure
         public Can()
         {
 
         }
 
+        //This... thing allows the user to set the flavor
         public Can (Flavor AFlavor)
         {
             TheFlavor = AFlavor;
