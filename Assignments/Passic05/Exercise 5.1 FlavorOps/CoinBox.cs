70e5b924793835289e0df43fb200cd6fb4995dec
Lars Passic <larspassic@outlook.com>
2021-02-23 20:17:14 -0800

    Create CoinBox.cs
    Init with base class framework and comments.

---
+++ Assignments/Passic05/Exercise 5.1 FlavorOps/CoinBox.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+//Assignment 05
+//Author: Passic, Lars, 2011958
+
+namespace Exercise_5._1_FlavorOps
+{
+    class CoinBox
+    {
+        private List<Coin> box;
+
+        //Constructor to create an empty coin box
+        public CoinBox()
+        {
+
+        }
+
+        //Constructor to create a coin box with some coins in it
+        public CoinBox(List<Coin> SeedMoney)
+        {
+
+        }
+
+        //Put a coin in the coin box
+        public  void Deposit(Coin ACoin)
+        {
+
+        }
+
+        //Take a coin of the specified denomination out of the box
+        public Boolean Withdraw(Coin.Denomination denomination)
+        {
+            bool result = false;
+
+
+
+            return result;
+        }
+
+        //Number of Half Dollars in the coin box
+        public int HalfDollarCount
+        {
+
+        }
+
+        //Number of quarters in the coin box
+        public int QuarterCount
+        {
+
+        }
+
+        //Number of dimes in the coin box
+        public int DimeCount
+        {
+
+        }
+
+        //Number of nickels in the coin box
+        public int NickelCount
+        {
+
+        }
+
+        //Number of worthless coins in the coin box
+        public int SlugCount
+        {
+
+        }
+
+        //Total amount of money in the coin box
+        public decimal ValueOf
+        {
+
+        }
+
+
+    }
+}
