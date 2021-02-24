83fc59350ca8eaa4502ec525e2ac72e25695d193
Lars Passic <larspassic@outlook.com>
2021-02-24 13:27:23 -0800

    Update CoinBox.cs
    minor updates to CoinBox comments and implementation

--- Assignments/Passic05/Exercise 5.1 FlavorOps/CoinBox.cs
+++ Assignments/Passic05/Exercise 5.1 FlavorOps/CoinBox.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
+using System.ComponentModel.Design;
 
 //Assignment 05
 //Author: Passic, Lars, 2011958
@@ -11,10 +13,12 @@ namespace Exercise_5._1_FlavorOps
     {
         private List<Coin> box;
 
+
         //Constructor to create an empty coin box
         public CoinBox()
         {
-
+            //Do something to establish the different coins at zero?
+            box = new List<Coin>();
         }
 
         //Constructor to create a coin box with some coins in it
@@ -24,12 +28,13 @@ namespace Exercise_5._1_FlavorOps
         }
 
         //Put a coin in the coin box
-        public  void Deposit(Coin ACoin)
+        public void Deposit(Coin ACoin)
         {
-
+            box.Add(ACoin);
         }
 
         //Take a coin of the specified denomination out of the box
+        //Need to use a LINQ query here
         public Boolean Withdraw(Coin.Denomination denomination)
         {
             bool result = false;
@@ -40,30 +45,39 @@ namespace Exercise_5._1_FlavorOps
         }
 
         //Number of Half Dollars in the coin box
+        //Need to use a LINQ query here
         public int HalfDollarCount
         {
+            get
+            {
+                return;
+            }
 
         }
 
         //Number of quarters in the coin box
+        //Need to use a LINQ query here
         public int QuarterCount
         {
 
         }
 
         //Number of dimes in the coin box
+        //Need to use a LINQ query here
         public int DimeCount
         {
 
         }
 
         //Number of nickels in the coin box
+        //Need to use a LINQ query here
         public int NickelCount
         {
 
         }
 
         //Number of worthless coins in the coin box
+        //Need to use a LINQ query here
         public int SlugCount
         {
 
@@ -75,6 +89,18 @@ namespace Exercise_5._1_FlavorOps
 
         }
 
+        public int CoinCount(Coin.Denomination ADenomination)
+        {
+            int coinCountFound = 0;
+            var findHalfDollars =
+            from possibleCandidate in box
+            where possibleCandidate.Count == ADenomination;
+            select possibleCandidate;
+
+            return coinCountFound;
+        }
+
 
     }
 }
+
