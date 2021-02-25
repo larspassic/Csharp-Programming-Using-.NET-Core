9bdbf7d4248e4bda2e04c9e6a9c69761d656680c
Lars Passic <larspassic@outlook.com>
2021-02-25 08:37:34 -0800

    Finished work on CoinCount method. Implemented CoinCount in all of the different coin properties as well.

--- Assignments/Passic05/Exercise 5.1 FlavorOps/CoinBox.cs
+++ Assignments/Passic05/Exercise 5.1 FlavorOps/CoinBox.cs
@@ -17,14 +17,14 @@ namespace Exercise_5._1_FlavorOps
         //Constructor to create an empty coin box
         public CoinBox()
         {
-            //Do something to establish the different coins at zero?
+            //I don't understand the difference between what line 14 is doing and what line 21 is doing.
             box = new List<Coin>();
         }
 
         //Constructor to create a coin box with some coins in it
         public CoinBox(List<Coin> SeedMoney)
         {
-
+            box = SeedMoney;
         }
 
         //Put a coin in the coin box
@@ -50,7 +50,7 @@ namespace Exercise_5._1_FlavorOps
         {
             get
             {
-                return;
+                return CoinCount(Coin.Denomination.HALFDOLLAR);
             }
 
         }
@@ -59,43 +59,69 @@ namespace Exercise_5._1_FlavorOps
         //Need to use a LINQ query here
         public int QuarterCount
         {
-
+            get
+            {
+                return CoinCount(Coin.Denomination.QUARTER);
+            }
         }
 
         //Number of dimes in the coin box
         //Need to use a LINQ query here
         public int DimeCount
         {
-
+            get
+            {
+                return CoinCount(Coin.Denomination.DIME);
+            }
+            
         }
 
         //Number of nickels in the coin box
         //Need to use a LINQ query here
         public int NickelCount
         {
-
+            get
+            {
+                return CoinCount(Coin.Denomination.NICKEL);
+            }
         }
 
         //Number of worthless coins in the coin box
         //Need to use a LINQ query here
         public int SlugCount
         {
-
+            get
+            {
+                return CoinCount(Coin.Denomination.SLUG);
+            }
         }
 
         //Total amount of money in the coin box
         public decimal ValueOf
         {
+            get
+            {
+                decimal totalValue = 0m;
 
+
+                return totalValue;
+            }
+            
         }
 
         public int CoinCount(Coin.Denomination ADenomination)
         {
+            //Create a variable to store the result
             int coinCountFound = 0;
-            var findHalfDollars =
+            
+            //LINQ query to find only coins that match the demonination that was passed in
+            var findCoins =
             from possibleCandidate in box
-            where possibleCandidate.Count == ADenomination;
+            where possibleCandidate.CoinEnumeral == ADenomination
             select possibleCandidate;
+
+            //Now store the count of the results as the int variable
+            coinCountFound = findCoins.Count();
 
             return coinCountFound;
         }
