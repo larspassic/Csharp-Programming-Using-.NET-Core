2a84f344f8b744af69cd22568b06873152893072
Lars Passic <larspassic@outlook.com>
2021-03-25 20:24:14 -0700

    Created Initial Food namespace and base classes

---
+++ Assignments/Passic09/Supplied 6.0 Vend Lib/Food.cs
@@ -0,0 +1,42 @@
+﻿
+//Assignment 09
+//Author: Passic, Lars, 2011958
+
+
+namespace Food
+{
+    class Snack
+    {
+
+    }
+
+    class HealthFood
+    {
+
+    }
+
+    class JunkFood
+    {
+
+    }
+
+    class Apple
+    {
+
+    }
+
+    class Banana
+    {
+
+    }
+
+    class CandyBar
+    {
+
+    }
+
+    class PotatoChips
+    {
+
+    }
+}
